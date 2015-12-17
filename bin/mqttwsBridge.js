0831a2f037930ec2b9b26064187bb5b8f1134c91
Agustin N. R. Ramirez <agustin107@gmail.com>
2015-12-17 18:58:15 -0300

    Corrige la forma en que se decodificada el mensaje

--- bin/mqttwsBridge.js
+++ bin/mqttwsBridge.js
@@ -7,176 +7,184 @@
  */
 
 var optimist = require('optimist'),
-    myArgs = optimist
-    .usage('MQTT/WebSocket Bridge\nUsage: $0')
-    .alias({
-        'p': 'port',
-        'h': 'host',
-        's': 'sslCert',
-        'k': 'sslKey',
-        'l': 'listen',
-        'c': 'configFile',
-    })
-    .describe({
-        's': 'Path to ssl certificate file',
-        'k': 'Path to ssl key file',
-        'p': 'MQTT port to connect to',
-        'h': 'Hostname of MQTT server',
-        'l': 'WebSocket port to listen on',
-        'c': 'Configuration file',
-        'help': 'Show this help'
-    })
-    .argv,
-    url = require('url'),
-    util = require('util'),
-    errno = require('errno'),
-    log4js = require('log4js'),
-    logger = log4js.getLogger(),
-    mqttws = require('../lib/mqtt-ws');
+  myArgs = optimist
+  .usage('MQTT/WebSocket Bridge\nUsage: $0')
+  .alias({
+    'p': 'port',
+    'h': 'host',
+    's': 'sslCert',
+    'k': 'sslKey',
+    'l': 'listen',
+    'c': 'configFile',
+  })
+  .describe({
+    's': 'Path to ssl certificate file',
+    'k': 'Path to ssl key file',
+    'p': 'MQTT port to connect to',
+    'h': 'Hostname of MQTT server',
+    'l': 'WebSocket port to listen on',
+    'c': 'Configuration file',
+    'help': 'Show this help'
+  })
+  .argv,
+  url = require('url'),
+  util = require('util'),
+  errno = require('errno'),
+  log4js = require('log4js'),
+  logger = log4js.getLogger(),
+  mqttws = require('../lib/mqtt-ws');
 
 if (myArgs.help) {
-    optimist.showHelp();
-    process.exit(0);
+  optimist.showHelp();
+  process.exit(0);
 }
 
 // If we are given a config file, parse that,
 // otherwise just parse command line
 if (myArgs.c || myArgs.configFile) {
-    var configFile = myArgs.c || myArgs.configFile;
-    logger.info("Reading configuration from %s", configFile);
-    require('fs').readFile(configFile, 'utf8', function(err, data) {
-        if (err) {
-            logger.info("Error reading config file %s: %s", configFile, err);
-            process.exit(-1);
-        }
-        config = JSON.parse(data);
-        run(parseCommandLine(myArgs, config));
-    });
+  var configFile = myArgs.c || myArgs.configFile;
+  logger.info("Reading configuration from %s", configFile);
+  require('fs').readFile(configFile, 'utf8', function(err, data) {
+    if (err) {
+      logger.info("Error reading config file %s: %s", configFile, err);
+      process.exit(-1);
+    }
+    config = JSON.parse(data);
+    run(parseCommandLine(myArgs, config));
+  });
 } else {
-    run(parseCommandLine(myArgs, {mqtt: {}, websocket: {}}));
+  run(parseCommandLine(myArgs, {
+    mqtt: {},
+    websocket: {}
+  }));
 }
 
 // Parse the command line
 function parseCommandLine(args, config) {
-    if (args.p || args.port) {
-        config.mqtt.port = args.p || args.port;
-    }
-    if (args.h || args.host) {
-        config.mqtt.host = args.h || args.host;
-    }
-    if (args.l || args.listen) {
-        config.websocket.port = args.l || args.listen;
-    }
-    if (args.s || args.sslCert) {
-        config.websocket.ssl_cert = args.s || args.sslCert;
-    }
-    if (args.k || args.sslKey) {
-        config.websocket.ssl_key = args.k || args.sslKey;
-    }
+  if (args.p || args.port) {
+    config.mqtt.port = args.p || args.port;
+  }
+  if (args.h || args.host) {
+    config.mqtt.host = args.h || args.host;
+  }
+  if (args.l || args.listen) {
+    config.websocket.port = args.l || args.listen;
+  }
+  if (args.s || args.sslCert) {
+    config.websocket.ssl_cert = args.s || args.sslCert;
+  }
+  if (args.k || args.sslKey) {
+    config.websocket.ssl_key = args.k || args.sslKey;
+  }
 
-    return config;
+  return config;
 }
 
 function getErrnoDescription(err) {
-    if (!err.errno) return undefined;
-    var e;
-    if (typeof err.errno == 'number') {
-        e = errno.errno[err.errno];
-        if (e) {
-            return e.description;
-        } else {
-            return undefined;
-        }
-    } else if (typeof err.errno == 'string') {
-        for (e in errno.errno) {
-            if (errno.errno[e].code == err.code) {
-                return errno.errno[e].description;
-            }
-        }
-        return undefined;
+  if (!err.errno) return undefined;
+  var e;
+  if (typeof err.errno == 'number') {
+    e = errno.errno[err.errno];
+    if (e) {
+      return e.description;
+    } else {
+      return undefined;
     }
+  } else if (typeof err.errno == 'string') {
+    for (e in errno.errno) {
+      if (errno.errno[e].code == err.code) {
+        return errno.errno[e].description;
+      }
+    }
+    return undefined;
+  }
 }
 
 function logError(err, message) {
-    if (err.syscall) {
-        var description = getErrnoDescription(err) || err.code;
-        logger.error("%s on %s: %s", message, err.syscall, description);
-    } else {
-        logger.error("%s: %s", message, err);
-    }
+  if (err.syscall) {
+    var description = getErrnoDescription(err) || err.code;
+    logger.error("%s on %s: %s", message, err.syscall, description);
+  } else {
+    logger.error("%s: %s", message, err);
+  }
 }
 
 // Start the bridge
 function run(config) {
-    if (config.log4js) {
-        log4js.configure(config.log4js);
-    }
+  if (config.log4js) {
+    log4js.configure(config.log4js);
+  }
 
-    // Create our bridge
-    bridge = mqttws.createBridge(config);
-    logger.info("Listening for incoming WebSocket connections on port %d",
-        bridge.port);
+  // Create our bridge
+  bridge = mqttws.createBridge(config);
+  logger.info("Listening for incoming WebSocket connections on port %d",
+    bridge.port);
 
-    // Set up error handling
-    bridge.on('error', function(err) {
-        logError(err, "WebSocket Error");
+  // Set up error handling
+  bridge.on('error', function(err) {
+    logError(err, "WebSocket Error");
+  });
+
+  // Handle incoming WS connection
+  bridge.on('connection', function(ws) {
+    // URL-decode the URL, and use the URI part as the subscription topic
+    logger.info("WebSocket connection from %s received", ws.connectString);
+
+    var self = this;
+
+    ws.on('error', function(err) {
+      logError(err, util.format("WebSocket error in client %s", ws.connectString));
     });
 
-    // Handle incoming WS connection
-    bridge.on('connection', function(ws) {
-        // URL-decode the URL, and use the URI part as the subscription topic
-        logger.info("WebSocket connection from %s received", ws.connectString);
+    // Parse the URL
+    var parsed = url.parse(ws.upgradeReq.url, true);
+    // Connect to the MQTT server using the URL query as options
+    var mqtt = bridge.connectMqtt(parsed.query);
+    mqtt.topic = decodeURIComponent(parsed.pathname.substring(1));
 
-        var self = this;
-
-        ws.on('error', function(err) {
-            logError(err, util.format("WebSocket error in client %s", ws.connectString));
-        });
-
-        // Parse the URL
-        var parsed = url.parse(ws.upgradeReq.url, true);
-        // Connect to the MQTT server using the URL query as options
-        var mqtt = bridge.connectMqtt(parsed.query);
-        mqtt.topic = decodeURIComponent(parsed.pathname.substring(1));
-
-        ws.on('close', function() {
-            logger.info("WebSocket client %s closed", ws.connectString);
-            mqtt.end();
-        });
-
-        ws.on('message', function(message) {
-            message = new Buffer(message);
-            var char = "";
-            var topic = "";
-            var offset = 0;
-            while(char != "|" && offset < message.length){
-                topic += char;
-                char = String.fromCharCode(message.readUInt16LE(offset));
-                offset += 2;
-            }
-            logger.info("WebSocket client %s publishing to %s", ws.connectString, topic);
-            mqtt.publish(topic, message.slice(offset), mqtt.options);
-        });
-
-        mqtt.on('error', function(err) {
-            logError(err, "MQTT error");
-        });
-
-        mqtt.on('connect', function() {
-            logger.info("Connected to MQTT server at %s:%d", mqtt.host, mqtt.port);
-            logger.info("WebSocket client %s subscribing to '%s'", ws.connectString, mqtt.topic);
-            mqtt.subscribe(mqtt.topic);
-        });
-
-        mqtt.on('close', function() {
-            logger.info("MQTT connection for client %s closed",
-                ws.connectString);
-            ws.terminate();
-        });
-
-        mqtt.on('message', function(topic, message, packet) {
-            ws.send(Buffer.concat([new Buffer(topic + "|", "utf16le"), new Buffer(message)]), {binary: true, mask: false});
-        });
+    ws.on('close', function() {
+      logger.info("WebSocket client %s closed", ws.connectString);
+      mqtt.end();
     });
+
+    ws.on('message', function(message) {
+      message = new Buffer(message);
+      var char = "";
+      var topic = "";
+      var offset = 0;
+      /*while (char != "|" && offset < message.length) {
+        console.log(offset);
+        topic += char;
+        char = String.fromCharCode(message.readUInt16LE(offset));
+        offset += 2;
+      }*/
+      topic = String.fromCharCode.apply(String, message);
+      logger.info("WebSocket client %s publishing to %s", ws.connectString, topic);
+      mqtt.publish(topic, message, mqtt.options);
+    });
+
+    mqtt.on('error', function(err) {
+      logError(err, "MQTT error");
+    });
+
+    mqtt.on('connect', function() {
+      logger.info("Connected to MQTT server at %s:%d", mqtt.host, mqtt.port);
+      logger.info("WebSocket client %s subscribing to '%s'", ws.connectString, mqtt.topic);
+      mqtt.subscribe(mqtt.topic);
+    });
+
+    mqtt.on('close', function() {
+      logger.info("MQTT connection for client %s closed",
+        ws.connectString);
+      ws.terminate();
+    });
+
+    mqtt.on('message', function(topic, message, packet) {
+      ws.send(Buffer.concat([new Buffer(topic + "|", "utf16le"), new Buffer(message)]), {
+        binary: true,
+        mask: false
+      });
+    });
+  });
 }
 
